af4e6bfff864225a9354569c2bcb6201252b0021
mll0032 <mllawson18@gmail.com>
2024-05-17 23:19:45 -0500

    Played with Hello, World!

--- hello.js
+++ hello.js
@@ -4,19 +4,34 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
-
+console.log("Hello, World!");
+console.log(1+4);
+console.log("Goodnight, World!");
+console.log("Hello, World!") + console.log("Good afternoon, World") + console.log("and Goodnight, World!");
+const greeting = "Hello,";
+const subject = " World! ";
+console.log(greeting + subject + greeting + subject)
+console.log('Quoth the Raven "Nevermore"');
+console.log('My Favoite quote is, "You may say I\'m a dreamer, but I\'m not the only one. I hope someday you\'ll join us. And the world will live as one." - John Lennon');
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
+      Changed from Hello to Hello, World!
    2. Figure out what the parentheses do. Will the code work without them?
+      Parenthese hold the information in this instance to be printed if they are not there the program does not output the string
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+   Removing quotations throws a syntax error I think it doesn't know what variable type is being sent. There seems to be no difference between ' vs "
    4. Remove the semi-colon, ;.
+   Nothing seems to go wrong but I do feel like that is the end command and maybe without it the program doesn't end exicution.
    5. Print a number. (Bonus: Print two numbers added together).
+   Numbers can be printed wihtout quotes and can have basic math operations to give the correct output
    6. Print multiple messages one after the other.
    7. Print two messages on the same line.
+   
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+   Found that you can seperate non quoted saying by using single and double quotation marks
    9. Other. You choose!
+   John Lennon Quote added
 */
